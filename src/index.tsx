ec7eb3bf57fc9f2adb6746f14e15b88f94a8c110
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2025-07-27 11:50:23 +0200

    remove buggy code

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { JSX, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import GameState from './gameState/GameState';
 
 const rootElement = document.querySelector('#root');
 if (!rootElement) throw new Error("Failed to find the root element");
@@ -17,9 +16,5 @@ function render(element: JSX.Element){
 }
 
 export default render;
-
-const gameState:GameState = new GameState(localStorage.getItem("gameState"));
-
-console.log(gameState);
 
 render(<App/>);
