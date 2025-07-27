608e7db794411e675a5767bc8c883fdae9aff459
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2025-07-27 11:43:52 +0200

    change name to `Secret Moonwolves`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-const App = () => (
-	<div>
-	 	<h1>Hello, World!</h1>
-	</div>
-);
+import render from "src";
+import "./main.css";
+
+function App(){
+	const name = "Secret Moonwolves";
+	return (
+		<div>
+			<h1 onClick={()=>{render(<h2>Clicked</h2>)}}>{name}</h1>
+		</div>
+	)
+};
 
 export default App;
  
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import { StrictMode } from 'react';
+import { JSX, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GameState from './gameState/GameState';
 
 const rootElement = document.querySelector('#root');
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = createRoot(rootElement);
 
-root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-);
+function render(element: JSX.Element){
+	root.render(
+		<StrictMode>
+			{element}
+		</StrictMode>
+	);
+}
+
+export default render;
+
+const gameState:GameState = new GameState(localStorage.getItem("gameState"));
+
+console.log(gameState);
+
+render(<App/>);
